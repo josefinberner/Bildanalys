5d5a4ead38c11bdef7c9baa967e3a0aa99bc05ed
jober <josefin.berner@knightec.se>
2018-03-12 17:17:03 +0100

    Cleaned up the code a bit

--- crossword.py
+++ crossword.py
@@ -4,30 +4,20 @@ import scipy.ndimage
 import copy
 from matplotlib import pyplot as plt, cm
 import itertools
-import matplotlib.patches as patches
 import pickle
 from sklearn import decomposition, neighbors
 
 
-def compare2plots(img1, img2):
-    fig, (subplot1, subplot2) = plt.subplots(ncols=2)
-    subplot1.imshow(img1, cmap=cm.gray)
-    subplot2.imshow(img2, cmap=cm.gray)
-
-
 def finding_lines(image, edges):
-
     from skimage.transform import probabilistic_hough_line
-    print(image.shape)
+
     # Line finding using the Probabilistic Hough Transform
-
-    lines = probabilistic_hough_line(edges, threshold=10, line_length=min(image.shape[0],image.shape[1])*0.2,
+    lines = probabilistic_hough_line(edges, threshold=10,
+                                     line_length=min(image.shape[0], image.shape[1])*0.2,
                                      line_gap=4)
-
     # Generating figure
     fig, axes = plt.subplots(1, 4, figsize=(15, 5), sharex=True, sharey=True)
     ax = axes.ravel()
-    print("got here")
     ax[0].imshow(image, cmap=cm.gray)
     ax[0].set_title('Input image')
 
@@ -44,7 +34,7 @@ def finding_lines(image, edges):
 
     ax[3].imshow(image)
     i_points = []
-    pairs = itertools.combinations(lines,2)
+    pairs = itertools.combinations(lines, 2)
     for line1, line2 in pairs:
         p1, p2 = line1
         p3, p4 = line2
@@ -52,12 +42,10 @@ def finding_lines(image, edges):
         if i_point[0] == float('inf'):
             continue
         i_points.append(i_point)
-    print('img shape:', image.shape)
-    print('corners: ', i_points.__len__())
 
     i_points_clean = clean_up_points(i_points)
     i_points = np.array(i_points_clean)
-    plt.scatter(i_points[:,0],i_points[:,1],s=10)
+    plt.scatter(i_points[:, 0], i_points[:, 1], s=10)
     ax[3].set_title('Corner points')
 
     for a in ax:
@@ -65,39 +53,57 @@ def finding_lines(image, edges):
         a.set_adjustable('box-forced')
 
     plt.tight_layout()
-    #plt.show()
     return lines, i_points
+
+def plot_result(img, predictions, squares, titles):
+    nbr_preds = predictions.shape[0]
+    fig, axes = plt.subplots(1, nbr_preds+1, figsize=(15,5), sharex=True, sharey=True)
+    ax = axes.ravel()
+    ax[0].imshow(img)
+    ax[0].set_title('Input image')
+
+    for ind in np.arange(1, nbr_preds+1):
+        img_prediction = copy.deepcopy(img)
+        preds = predictions[ind-1, :]
+        title = titles[ind-1, :]
+        for sq_nbr in np.flatnonzero(preds == 3):
+            color_square(sq_nbr, [0, 0, 250], img_prediction, squares)
+
+        for sq_nbr in np.flatnonzero(preds == 1):
+            color_square(sq_nbr, [0, 250, 0], img_prediction, squares)
+
+        ax[ind].imshow(img_prediction)
+        ax[ind].set_title(title)
+
+    for a in ax:
+        a.set_axis_off()
+        a.set_adjustable('box-forced')
+
+    plt.tight_layout()
+
 
 def clean_up_points(points):
     nbr_of_points = points.__len__()
-    point_pairs = itertools.combinations(points,2)
-    to_remove = {} # Creates a dictonary
+    point_pairs = itertools.combinations(points, 2)
+    to_remove = {}  # Creates a dictonary
     threshold = 10
     ok_points = 0
     bad_points = 0
     nbr_of_pairs = 0
     for p1, p2 in point_pairs:
-        nbr_of_pairs = nbr_of_pairs +1
+        nbr_of_pairs = nbr_of_pairs + 1
         x1, y1 = p1
         x2, y2 = p2
         if p1 in to_remove:
             new_p1 = to_remove.get(p1)
             x1, y1 = new_p1
-            #print(new_p1)
         if p2 in to_remove:
             new_p2 = to_remove.get(p2)
             x2, y2 = new_p2
-            #print(new_p2)
-        # Skapa ett uppslagsverk istället!
-            # Kolla om p1 eller p2 finns i uppslagsverket
-            # isf ersätt med ny punkt
-            # gör kollen om nära varandra
-            # isf lägg till i uppslagsverket (key=old_p, value = new_p)
-            # när klart, ersätt punkter i listan med de nya från uppslagsverket
         if abs(x1-x2) < threshold:
             if abs(y1-y2) < threshold:
                 bad_points = bad_points + 1
-                new_point = (int((x1+x2)/2),int((y1+y2)/2))
+                new_point = (int((x1+x2)/2), int((y1+y2)/2))
                 to_remove[p1] = new_point
                 to_remove[p2] = new_point
         ok_points = ok_points + 1
@@ -109,10 +115,10 @@ def clean_up_points(points):
             continue
         points.append(value)
     new_length = points.__len__()
-    # print('After removing and adding', new_length)
     if nbr_of_points == new_length:
         return points
-    else: clean_up_points(points)
+    else:
+        clean_up_points(points)
     return points
 
 
@@ -125,35 +131,35 @@ def get_intersect(a1, a2, b1, b2):
     b2: [x, y] another point on the second line
     """
     x1=a1[0]; x2=a2[0];
-    xmin = min(x1,x2)
-    xmax = max(x1,x2)
-    s = np.vstack([a1,a2,b1,b2])        # s for stacked
-    h = np.hstack((s, np.ones((4, 1)))) # h for homogeneous
+    xmin = min(x1, x2)
+    xmax = max(x1, x2)
+    s = np.vstack([a1, a2, b1, b2])        # s for stacked
+    h = np.hstack((s, np.ones((4, 1))))  # h for homogeneous
     l1 = np.cross(h[0], h[1])           # get first line
     l2 = np.cross(h[2], h[3])           # get second line
     x, y, z = np.cross(l1, l2)          # point of intersection
     if z == 0:                          # lines are parallel
-        return (float('inf'), float('inf'))
+        return float('inf'), float('inf')
     if xmin <= x/z <= xmax:
-        return (x/z, y/z)
-    return (float('inf'),float('inf'))
+        return x/z, y/z
+    return float('inf'), float('inf')
 
 
 def finding_squares(img, img_col):
     lines, corner_points = finding_lines(img_col, img)
-    squares=np.array([0, 0, 0, 0],dtype=int)
+    squares=np.array([0, 0, 0, 0], dtype=int)
     for corner in corner_points:
         upp_right = closest_right(corner, corner_points)
-        if upp_right.any() == None:
-           # print('no point to the right of', corner)
+        if upp_right.any() is None:
+            # print('no point to the right of', corner)
             continue
         down_left = closest_down(corner, corner_points)
-        if down_left.any() == None:
-           # print('no point below', corner)
+        if down_left.any() is None:
+            # print('no point below', corner)
             continue
-        square = np.array([corner[0],upp_right[0],corner[1],down_left[1]],dtype=int)
+        square = np.array([corner[0], upp_right[0], corner[1], down_left[1]], dtype = int)
         squares = np.vstack([squares, square])
-    squares = squares[1:,:]
+    squares = squares[1:, :]
     print('nbr of squares', squares.__len__())
     return squares
 
@@ -161,32 +167,31 @@ def finding_squares(img, img_col):
 def closest_down(corner, corner_points):
     closest_dist = 100000
     alignment_threshold = 20
-    closest_down = np.array([None, None])
+    cl_down = np.array([None, None])
     for c in corner_points:
         if c[1] > corner[1]:
             if abs(c[0]-corner[0]) < alignment_threshold:
                 if c[1] - corner[1] < closest_dist:
                     closest_dist = c[1]-corner[1]
-                    closest_down = c
-    return closest_down
+                    cl_down = c
+    return cl_down
 
 
 def closest_right(corner, corner_points):
     closest_dist = 100000
     alignment_threshold = 20
-    closest_right = np.array([None, None])
+    cl_right = np.array([None, None])
     for c in corner_points:
         if c[0] > corner[0]:
             if abs(c[1]-corner[1]) < alignment_threshold:
                 if c[0] - corner[0] < closest_dist:
                     closest_dist = c[0]-corner[0]
-                    closest_right = c
-    return closest_right
+                    cl_right = c
+    return cl_right
 
 
 def color_square(nbr, col_code, img, square_points):
     [row1, row2, col1, col2] = square_points[nbr, :]
-    # print(row1,row2,col1,col2)
     img[col1:col2, row1:row2] = col_code  # Array indexing x1:x2,y1:y2
 
 
@@ -195,7 +200,6 @@ def sq_points_2_X(sq, img):
     # and the original grayscale image
 
     f_sq = sq[100, :]
-    print(f_sq)
     first_el = img[f_sq[0]:f_sq[1], f_sq[2]:f_sq[3]]
     STANDARD_SIZE = first_el.shape
     print('STANDARD_SIZE', STANDARD_SIZE)
@@ -208,21 +212,19 @@ def sq_points_2_X(sq, img):
         X = np.vstack([X, img_el.flatten()])
 
     print('X is: ', X.shape)
-    return [X[1:,:], STANDARD_SIZE]
+    return [X[1:, :], STANDARD_SIZE]
 
 
 def classify_squares_features(X, STANDARD_SIZE):
     nbr_of_features = 7
     features = np.zeros([X.shape[0], nbr_of_features])
     total_nbr_pixels = STANDARD_SIZE[0]*STANDARD_SIZE[1]
-    print('Pixels: ', total_nbr_pixels)
-    print('Image size', STANDARD_SIZE)
 
     for ind in np.arange(0, X.shape[0]):
         img_el = np.reshape(X[ind, :], STANDARD_SIZE)
 
     # feature 1 - average pixel intensity
-        f1 = img_el.sum() / total_nbr_pixels #/ 255 #normalized to a number from 0-1
+        f1 = img_el.sum() / total_nbr_pixels  # / 255 #normalized to a number from 0-1
     # feature 2 - total number of "white" pixels div by total number of pixels
         whites = img_el > 200
         f2 = np.sum(whites) / total_nbr_pixels
@@ -237,153 +239,76 @@ def classify_squares_features(X, STANDARD_SIZE):
         f6 = np.sum(blacks.sum(axis=1) > 0) / STANDARD_SIZE[0]
     # feature 7 - average of cols with (some) black pixels
         f7 = np.sum(blacks.sum(axis=0) > 0) / STANDARD_SIZE[1]
-        features[ind,:] = [f1, f2, f3, f4, f5, f6, f7]
+        features[ind, :] = [f1, f2, f3, f4, f5, f6, f7]
     return features
-
-
-def classify_squares_train(X, y):
-
-    pca = decomposition.PCA(n_components=5, svd_solver='randomized')
-    # Vet inte skillnaden på randomized och inte, borde kolla upp
-    X = pca.fit_transform(X)
-    print(X.shape)
-    print('yshape', y.shape)
-
-    plotting = False
-    if plotting:
-        plt.figure(4)
-        plt.scatter(X[:, 0], X[:, 1], c=y)
-        plt.colorbar()
-        plt.figure(5)
-        plt.scatter(X[:, 2], X[:, 3], c=y)
-        plt.colorbar()
-        plt.figure(6)
-        plt.scatter(X[:, 0], X[:, 4], c=y)
-        plt.colorbar()
-
-    knn = neighbors.KNeighborsClassifier()
-    knn.fit(X, y)
-    return [pca, knn]
-
-
-def classify_squares_predict(X, knn):
-    preds = knn.predict(X)
-    return preds
-    # Want to classify the squares into one of the following
-    # 0 = Empty square
-    # 1 = Lead square
-    # 2 = Filled square
-    # 3 = Image/Other/unknown
-
-
-def get_targets(img_color, squares):
-    with open('training_data.pkl', 'rb') as f:
-        squares, targets, training_img = pickle.load(f)
-    print(squares.shape, targets.shape)
-    print(targets)
-    return targets
 
 
 def read_process_image(img_file):
     image = io.imread(img_file, as_grey=True)
     img_gray = scipy.ndimage.imread(img_file, mode='L')
     img_color = scipy.ndimage.imread(img_file, mode='RGB')
-    # plt.hist(image)
-    # edges = feature.canny(image, sigma=2, low_threshold=0, high_threshold=0.1)
     edges = feature.canny(image, sigma=0.5, low_threshold=0, high_threshold=0)
     return [img_gray, img_color, edges]
 
 
-def plot_result(img, preds, squares, title):
-    img_prediction = copy.deepcopy(img)
-    for sq_nbr in np.flatnonzero(preds == 3):
-        color_square(sq_nbr, [0,0,250], img_prediction, squares)
-
-    for sq_nbr in np.flatnonzero(preds == 1):
-        color_square(sq_nbr, [0,250,0], img_prediction, squares)
-    compare2plots(img, img_prediction)
-    plt.title(title)
-
-
-def main():
-    img_file = 'DN.Korsord.jpg'
-    [img_gray, img_color, edges] = read_process_image(img_file)
-    squares = finding_squares(edges, img_color)
-    print('nbr of squares returned', squares.shape)
-    targets = get_targets(img_color, squares)
-
-    [X, STANDARD_SIZE] = sq_points_2_X(squares, img_gray)
-
+def get_predictor(filename):
+    with open(filename, 'rb') as f:
+        squares, targets, training_img = pickle.load(f)
+    [X, STANDARD_SIZE] = sq_points_2_X(squares, training_img)
     feature_vec = classify_squares_features(X, STANDARD_SIZE)
+
+    # Feature vector
     knn = neighbors.KNeighborsClassifier(n_neighbors=10)
     knn.fit(feature_vec, targets)
-    preds = classify_squares_predict(feature_vec, knn)
 
-    # Prediction results
-    plot_result(img_color, preds, squares, 'Training data features')
-    plot_squares(img_color, squares)
-
-
-    ############ PCA ##############
-    # PCA of feature-vector
+    # First component PCA on feature vector
     knn_pca = neighbors.KNeighborsClassifier(n_neighbors=10)
     pca = decomposition.PCA(n_components=1, svd_solver='randomized')
     pcaX = pca.fit_transform(feature_vec)
     knn_pca.fit(pcaX, targets)
-    preds_pca = classify_squares_predict(pcaX, knn_pca)
-    # Prediction results
-    plot_result(img_color, preds_pca, squares, 'PCA on feature vector')
-    ###############################
 
-    # Validation data
-    img_file = 'DN.Korsord2.jpg'
-    [img_gray2, img_color2, edges2] = read_process_image(img_file)
-    squares2 = finding_squares(edges2, img_color2)
-    [X, STANDARD_SIZE] = sq_points_2_X(squares2, img_gray2)
-    feature_vec = classify_squares_features(X, STANDARD_SIZE)
-    preds2 = classify_squares_predict(feature_vec, knn)
-    plot_result(img_color2, preds2, squares2, 'Validation')
+    # PCA only
+    pca_only = decomposition.PCA(n_components=5, svd_solver='randomized')
+    X = pca_only.fit_transform(X)
+    knn_pca_only = neighbors.KNeighborsClassifier()
+    knn_pca_only.fit(X, targets)
 
-    pcaX = pca.fit_transform(feature_vec)
-    preds_pca = classify_squares_predict(pcaX, knn_pca)
-    plot_result(img_color2, preds_pca, squares2, 'Validation PCA features')
+    return knn, knn_pca, pca, pca_only, knn_pca_only
 
 
-def main_pca():
-    img_file = 'DN.Korsord.jpg'
+def classify_crossword(img_file, titleline):
+
     [img_gray, img_color, edges] = read_process_image(img_file)
-    squares = finding_squares(edges)
-    targets = get_targets(img_color, squares)
+    squares = finding_squares(edges, img_color)
+    knn, knn_pca, pca, pca_only, knn_pca_only = get_predictor('training_data.pkl')
     [X, STANDARD_SIZE] = sq_points_2_X(squares, img_gray)
-    print('X1', X.shape)
-    [pca, knn] = classify_squares_train(X, targets)
-    pcaX = pca.fit_transform(X)
-    preds = classify_squares_predict(pcaX, knn)
-    plot_result(img_color, preds, squares, 'Training data PCA')
+    feature_vec = classify_squares_features(X, STANDARD_SIZE)
 
-    # Validation
-    img_file = 'DN.Korsord2.jpg'
-    [img_gray2, img_color2, edges2] = read_process_image(img_file)
-    squares2 = finding_squares(edges2)
-    [X, STANDARD_SIZE] = sq_points_2_X(squares2, img_gray2)
-    print('X2', X.shape)
-    pcaX = pca.fit_transform(X)
-    preds2 = classify_squares_predict(pcaX, knn)
-    plot_result(img_color2, preds2, squares2, 'Validation data PCA')
+    ######## Feature vector #########
+    preds = knn.predict(feature_vec)
+    title = titleline + ' features'
+    # plot_result(img_color, preds, squares, titleline + ' features')
+
+    ########## PCA on feature vec ############
+    pcaX = pca.fit_transform(feature_vec)
+    preds = np.vstack([preds, knn_pca.predict(pcaX)])
+    title = np.vstack([title, titleline + ' PCA features'])
+    # plot_result(img_color, preds_pca, squares, titleline + ' PCA features')
+
+    ######### Only PCA ##########
+    pcaX_only = pca_only.fit_transform(X)
+    preds = np.vstack([preds, knn_pca_only.predict(pcaX_only)])
+    title = np.vstack([title, titleline + ' PCA only'])
+
+    plot_result(img_color, preds, squares, title)
 
 
+def main():  # TODO: Make it possible to run file with image file as argument
 
-def plot_squares(img, squares):
-    img_squares = copy.deepcopy(img)  # To not only make a new reference binding to the same object
-    for sq_nbr in np.arange(0, squares.shape[0] - 3, 4):
-        color_square(sq_nbr, [1, 100, 1], img_squares, squares)
-        color_square(sq_nbr + 1, [100, 1, 100], img_squares, squares)
-        color_square(sq_nbr + 2, [1, 1, 100], img_squares, squares)
-        color_square(sq_nbr + 3, [100, 1, 1], img_squares, squares)
-    compare2plots(img, img_squares)
+    classify_crossword('DN.Korsord.jpg', 'Training data')
+    classify_crossword('DN.Korsord2.jpg', 'Validation')
 
 
 if __name__ == '__main__':
     main()
-    # main_pca()
     plt.show(block=True)
